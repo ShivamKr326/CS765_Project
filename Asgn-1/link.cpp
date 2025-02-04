81ed9f10f0a34125e076ecc3b57ba34a5c7fb295
Shashi Vardhan Pothulwar <115585527+shashi250505@users.noreply.github.com>
2025-02-04 12:23:44 +0530

    Update link.cpp

--- Asgn-1/link.cpp
+++ Asgn-1/link.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-Link::Link(Peer* p, bool isfast, int light_delay){
+Link::Link(Peer* p, bool isfast, ld light_delay){
     pij = light_delay;
     peer = p;
     if(peer->isfast && isfast) cij = 12500;
     else cij = 625;
-    dij = exponential_distribution<ld>(cij/12);
+    dij = exponential_distribution<ld> (cij/12);
 }
 
-int Link::delay(int msg_length){
+ld Link::delay(int msg_length){
     ld total_delay = (ld)pij + (ld)msg_length/cij + dij(gen);
-    return (int)total_delay;
+    return total_delay;
 }
